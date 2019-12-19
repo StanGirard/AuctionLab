065e6a9496a707b790338900aec346653111e601
Antoine Dewez <dewez.antoine2@gmail.com>
2019-12-19 17:15:35 +0100

    menu CSS

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { InlineSVGModule } from 'ng-inline-svg';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MenuComponent } from './components/menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InlineSVGModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/components/menu/menu.component.ts
+++ front/src/app/components/menu/menu.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+
+  activeMenuItems = {
+    clients: false,
+    inventaires: false,
+    encheres: false,
+    produits: false
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickOnMenuItem(menuItem: string) {
+    this.activeMenuItems[menuItem] = !this.activeMenuItems[menuItem];
+  }
 }
