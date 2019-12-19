848d74d4f36ee1fe951ab3f6112ada8a3014b39c
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-19 16:44:10 +0100

    Create user

--- back/libs/response-lib.js
+++ back/libs/response-lib.js
@@ -1,9 +1,9 @@
-export function success(body) 
-{ 
+export function success(body)
+{
     return buildResponse(200, body);
 }
-export function failure(body) 
-{ 
+export function failure(body)
+{
     return buildResponse(500, body);
 }
 function buildResponse(statusCode, body) { return {
