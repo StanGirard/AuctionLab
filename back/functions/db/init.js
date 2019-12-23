5fd3d1e474c6f9a9821edecb33cda67f2d0d6446
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-23 22:46:42 +0100

    [AURORA] Changed Users to Clients

--- back/functions/db/init.js
+++ back/functions/db/init.js
@@ -1,7 +1,7 @@
 import { success, failure } from '../../libs/response-lib';
 import { AuroraDB } from '../../libs/aurora-lib';
 
-const createUser = "CREATE TABLE IF NOT EXISTS MagaDB.User ( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, nom VARCHAR(100), prenom VARCHAR(100), email VARCHAR(255), pays VARCHAR(255), ville VARCHAR(255), code_postal VARCHAR(5), addresse VARCHAR(255), acheteur BOOLEAN, vendeur BOOLEAN, created_at DATE);";
+const createUser = "CREATE TABLE IF NOT EXISTS MagaDB.Clients ( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, nom VARCHAR(100), prenom VARCHAR(100), email VARCHAR(255), pays VARCHAR(255), ville VARCHAR(255), code_postal VARCHAR(5), addresse VARCHAR(255), acheteur BOOLEAN, vendeur BOOLEAN, created_at DATE);";
 
 exports.handler = async (event, context) => {
     try {
--- back/functions/user/create.js
+++ back/functions/user/create.js
@@ -1,24 +1,15 @@
 import { success, failure } from "../../libs/response-lib";
 import { AuroraDB } from '../../libs/aurora-lib';
 
-/*
-const clean = (string) => {
-  if (string) {
-    return string;
-  } else {
-    return "";
-  }
-};*/
-
 export async function main(event, context) {
   try {
     const data = JSON.parse(event.body);
-    const verifyQuery = "SELECT * from User where email = :email;";
+    const verifyQuery = "SELECT * from Clients where email = :email;";
     let resultVerify = await AuroraDB.query(verifyQuery, {email:data.email});
     if (resultVerify.records.length > 0){
       return success({status: true, result: [], error: "Email already used"});
     } else {
-      const query = `INSERT INTO User (nom, prenom, email, pays, ville, code_postal, addresse, acheteur, vendeur, created_at) VALUES(:nom, :prenom, :email, :pays, :ville, :code_postal, :addresse, :acheteur, :vendeur, now())`;
+      const query = `INSERT INTO Clients (nom, prenom, email, pays, ville, code_postal, addresse, acheteur, vendeur, created_at) VALUES(:nom, :prenom, :email, :pays, :ville, :code_postal, :addresse, :acheteur, :vendeur, now())`;
       let result = await AuroraDB.query(query,{
         nom: data.nom, prenom: data.prenom, email: data.email, pays: data.pays,
         ville: data.ville, code_postal: data.code_postal, addresse: data.addresse, acheteur: data.acheteur, vendeur: data.vendeur, created_at: "now()"});
@@ -26,7 +17,6 @@ export async function main(event, context) {
     }
 } catch (e) {
     console.log(e);
-    //return failure({ status: false, error: e });
     return failure({status: false, error: e});
 }
 }
