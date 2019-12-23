8ee88f8ade8997bfd7de753b85c6c55ff799de08
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-23 23:45:23 +0100

    [SERVERLESS] Changed folder structure

---
+++ back/functions/clients/update.js
@@ -0,0 +1,16 @@
+import { success, failure } from "../../libs/response-lib";
+import { AuroraDB } from '../../libs/aurora-lib';
+
+export async function main(event, context) {
+    try {
+        const query = `INSERT INTO Clients (identifier,nom, prenom, email, pays, ville, code_postal, addresse, acheteur, vendeur, created_at) VALUES(:identifier, :nom, :prenom, :email, :pays, :ville, :code_postal, :addresse, :acheteur, :vendeur, now())`;
+        let result = await AuroraDB.query(query, {
+            identifier: event.pathParameters.id, nom: data.nom, prenom: data.prenom, email: data.email, pays: data.pays,
+            ville: data.ville, code_postal: data.code_postal, addresse: data.addresse, acheteur: data.acheteur, vendeur: data.vendeur, created_at: "now()"
+        });
+        return success({ status: true, result: result });
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false, error: e });
+    }
+}
