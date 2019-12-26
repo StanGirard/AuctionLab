b82c951d47864f5eda38b27a409d1a8f45a6d207
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-26 12:52:22 +0100

    [LAMBDA][EMPLOYE] Get & Get list added

---
+++ back/functions/employe/get.js
@@ -0,0 +1,15 @@
+import { success, failure } from "../../libs/response-lib";
+import { AuroraDB } from '../../libs/aurora-lib';
+
+export async function main(event, context) {
+    try {
+        const query = `SELECT * from Employe where id = :id`;
+        let result = await AuroraDB.query(query, {
+            id: event.pathParameters.id
+        });
+        return success({ status: true, result: result });
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false, error: e });
+    }
+}
