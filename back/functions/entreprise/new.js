873663c8b87a2a833ddb76e260b5c54d2f9a53a1
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-26 09:04:34 +0100

    [LAMBDA] Updated transaction -> commit

--- back/functions/entreprise/new.js
+++ back/functions/entreprise/new.js
@@ -4,14 +4,14 @@ import { AuroraDB } from '../../libs/aurora-lib';
 export async function main(event, context) {
   try {
     const data = JSON.parse(event.body);
-    const verifyQuery = "SELECT * from Clients where identifier = :identifier;";
+    const verifyQuery = "SELECT * from Entreprise where identifier = :identifier;";
     let resultVerify = await AuroraDB.query(verifyQuery, {identifier: event.pathParameters.id});
     if (resultVerify.records.length > 0){
       return failure({status: false, result: [], error: "Identifier already used"});
     } else {
-      const query = `INSERT INTO Clients (identifier,denomination, entreprise, acheteur, vendeur, vip, created_at) VALUES(:identifier,:denomination, :entreprise,:acheteur, :vendeur,:vip,  now())`;
-      let result = await AuroraDB.query(query,{
-        identifier: event.pathParameters.id, denomination: data.denomination, entreprise: data.entreprise, acheteur: data.acheteur, vendeur: data.vendeur, vip: data.vip, created_at: "now()"})
+      const query = `INSERT INTO Entreprise (identifier,numero_rcs, numero_tva, telephone, email) VALUES (:identifier,:numero_rcs, :numero_tva,:telephone, :email)`;
+      let result = await AuroraDB.transaction().query(query,{
+        identifier: event.pathParameters.id, numero_rcs: data.numero_rcs, numero_tva: data.numero_tva, telephone: data.telephone, email: data.email})
         .rollback((e,status) => { failure({status: false, result: status, error: e}); })
         .commit();
         return success({status:true, result:result});
--- back/functions/particulier/new.js
+++ back/functions/particulier/new.js
@@ -4,14 +4,14 @@ import { AuroraDB } from '../../libs/aurora-lib';
 export async function main(event, context) {
   try {
     const data = JSON.parse(event.body);
-    const verifyQuery = "SELECT * from Clients where identifier = :identifier;";
+    const verifyQuery = "SELECT * from Particulier where identifier = :identifier;";
     let resultVerify = await AuroraDB.query(verifyQuery, {identifier: event.pathParameters.id});
     if (resultVerify.records.length > 0){
       return failure({status: false, result: [], error: "Identifier already used"});
     } else {
-      const query = `INSERT INTO Clients (identifier,denomination, entreprise, acheteur, vendeur, vip, created_at) VALUES(:identifier,:denomination, :entreprise,:acheteur, :vendeur,:vip,  now())`;
+      const query = `INSERT INTO Particulier (identifier,titre, email, telephone, nom, prenom, nationalite, numero_identite) VALUES (:identifier,:titre, :email,:telephone, :nom,:prenom, :nationalite, :numero_identite)`;
       let result = await AuroraDB.query(query,{
-        identifier: event.pathParameters.id, denomination: data.denomination, entreprise: data.entreprise, acheteur: data.acheteur, vendeur: data.vendeur, vip: data.vip, created_at: "now()"})
+        identifier: event.pathParameters.id, titre: data.titre, email: data.email, telephone: data.telephone, nom: data.nom, prenom: data.prenom, nationalite: data.nationalite, numero_identite: data.numero_identite  })
         .rollback((e,status) => { failure({status: false, result: status, error: e}); })
         .commit();
         return success({status:true, result:result});
