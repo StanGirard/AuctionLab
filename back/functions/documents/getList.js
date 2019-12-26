6e0808394374d86c6c18d5009a19aabb667e08d9
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-26 13:22:51 +0100

    [LAMBDA] GetList Added array in filter

--- back/functions/documents/getList.js
+++ back/functions/documents/getList.js
@@ -12,21 +12,41 @@ export async function main(event, context) {
                 const filters = JSON.parse(params.filter);
                 var number = 0;
                 for (const filt in filters) {
-                    if (number != 0) { query += " AND "; }
-                    number++;
-                    if (filters[filt] == "true" || filters[filt] == "false"){
-                        query += filt + " IS " + filters[filt] + " ";
+                    if (!Array.isArray(filters[filt])) {
+                        if (number != 0) { query += " AND "; }
+                        number++;
+                        if (filters[filt] == "true" || filters[filt] == "false") {
+                            query += filt + " IS " + filters[filt] + " ";
+                        } else {
+                            query += filt + " LIKE '%" + filters[filt] + "%' ";
+                        }
                     } else {
-                        query += filt + " LIKE '%" + filters[filt] + "%' ";
+                        if (number != 0) { query += " AND "; }
+                        number++;
+                        query += filt + " IN " + "(";
+                        const arr = filters[filt];
+                        var number2 = 0;
+                        for (var val in arr){
+                            var el = val;
+                            if (number2 != 0) {
+                                query += ",";
+                            }
+                            if (typeof arr[val] === 'string') {
+                                 el = "'" + arr[val] + "'";
+                            }
+                            query += el;
+                            number2++;
+                        }
+                        query += ") ";
                     }
                 }
             }
-            if (params.sort != null){
+            if (params.sort != null) {
                 // sort example: sort=["identifier","DESC"]
                 const sort = JSON.parse(params.sort);
                 query += "ORDER BY " + sort[0] + " " + sort[1] + " ";
             }
-            if (params.range != null){
+            if (params.range != null) {
                 //range example: range=[0, 1]
                 const range = JSON.parse(params.range);
                 query += " LIMIT " + range[0] + " , " + range[1];
