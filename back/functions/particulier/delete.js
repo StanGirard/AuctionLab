86d704a980a9764df6caedd59ea47b533773e2c0
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-26 13:55:40 +0100

    [LAMBDA][DELETE] Delete added for all clients tabl

---
+++ back/functions/particulier/delete.js
@@ -0,0 +1,15 @@
+import { success, failure } from "../../libs/response-lib";
+import { AuroraDB } from '../../libs/aurora-lib';
+
+export async function main(event, context) {
+    try {
+        const query = `DELETE FROM  Particulier where identifier = :identifier;`;
+        let result = await AuroraDB.query(query, {
+            identifier: event.pathParameters.id
+        });
+        return success({ status: true, result: result });
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false, error: e });
+    }
+}
