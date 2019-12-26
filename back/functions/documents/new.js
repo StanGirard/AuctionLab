18f4168c4d9ad4f8f657c3d4c04f8cb2767ca921
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-26 09:45:09 +0100

    [LAMBDA] Document Create

---
+++ back/functions/documents/new.js
@@ -0,0 +1,23 @@
+import { success, failure } from "../../libs/response-lib";
+import { AuroraDB } from '../../libs/aurora-lib';
+
+export async function main(event, context) {
+  try {
+    const data = JSON.parse(event.body);
+    const verifyQuery = "SELECT * from Documents where type = :type AND identifier = :identifier;";
+    let resultVerify = await AuroraDB.query(verifyQuery, {type: data.type, identifier: event.pathParameters.id});
+    if (resultVerify.records.length > 0){
+      return failure({status: false, result: [], error: "Type already used"});
+    } else {
+      const query = `INSERT INTO Documents (identifier,type, url, date_added) VALUES(:identifier,:type, :url,now())`;
+      let result = await AuroraDB.transaction().query(query,{
+        identifier: event.pathParameters.id, type: data.type, url: data.url})
+        .rollback((e,status) => { failure({status: false, result: status, error: e}); })
+        .commit();
+        return success({status:true, result:result});
+    }
+} catch (e) {
+    console.log(e);
+    return failure({status: false, error: e});
+}
+}
