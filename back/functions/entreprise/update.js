5217cfbc56de43a5ea632dd372f35f876e206cf5
Stanislas Girard <girard.stanislas@gmail.com>
2019-12-29 22:44:38 +0100

    [AURORA] Corrected an error Entreprise den

--- back/functions/entreprise/update.js
+++ back/functions/entreprise/update.js
@@ -4,9 +4,9 @@ import { AuroraDB } from '../../libs/aurora-lib';
 export async function main(event, context) {
     try {
         const data = JSON.parse(event.body);
-        const query = `UPDATE Entreprise SET   numero_rcs = :numero_rcs, numero_tva = :numero_tva, telephone = :telephone, email = :email where identifier = :identifier;`;
+        const query = `UPDATE Entreprise SET  denomination= :denomination, numero_rcs = :numero_rcs, numero_tva = :numero_tva, telephone = :telephone, email = :email where identifier = :identifier;`;
         let result = await AuroraDB.query(query, {
-            identifier: event.pathParameters.id, numero_rcs: data.numero_rcs, numero_tva: data.numero_tva,
+            identifier: event.pathParameters.id, denomination: data.denomination, numero_rcs: data.numero_rcs, numero_tva: data.numero_tva,
             telephone: data.telephone, email: data.email
         });
         return success({ status: true, result: result });
